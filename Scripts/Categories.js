349d9c093d8467dcc09bd18596a2e0797edc9a81
PhamSyNhatNhan <phamsynhatnhan35@gmail.com>
2023-10-19 11:30:43 +0700

    first commit

---
+++ Scripts/Categories.js
@@ -0,0 +1,140 @@
+$(".i_header_1 > span").click(function () {
+    var $languageContent = $(this).find(".i_language_content");
+    $languageContent.toggle(50);
+});
+
+$(".i_header_1 > span").mouseleave(function () {
+    var $languageContent = $(this).find(".i_language_content");
+    $languageContent.hide(50);
+});
+
+$(".i_language_content").mouseenter(function () {
+    $(this).stop(true, true);
+});
+
+$(document).ready(function() {
+    var $ImainContainers = $('.ct_container');
+
+    $ImainContainers.each(function() {
+        var container = $(this);
+        var slider = container.find('.slider');
+        var slides = slider.children('li');
+        var progress = container.find('.progress');
+        var progressContainer = container.find('.progress_bar_sub');
+        var prevButton = container.find('.slider-controls > .prev-button_ct');
+        var nextButton = container.find('.slider-controls > .next-button_ct');
+
+        let currentSlideIndex = 0;
+        let autoAdvanceTimer;
+        let slideDuration = 2000; 
+
+        function goToSlide(index) {
+            if (index >= 0 && index < slides.length) {
+            slides.css('display', 'none');
+            slides.eq(index).css('display', 'block');
+            }
+        }
+        
+        function nextSlide() {
+            currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+            goToSlide(currentSlideIndex);
+            updateProgressBar();
+        }
+
+        function prevSlide() {
+            currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+            goToSlide(currentSlideIndex);
+            updateProgressBar();
+        }
+        
+        
+        nextButton.click(nextSlide);
+        prevButton.click(prevSlide);
+
+        var slideshowContainer = container;
+
+        slideshowContainer.on('mouseover', function() {
+            clearInterval(autoAdvanceTimer);
+        });
+
+        slideshowContainer.on('mouseout', function() {
+            autoAdvanceTimer = setInterval(nextSlide, slideDuration);
+        });
+
+        autoAdvanceTimer = setInterval(nextSlide, slideDuration);
+
+        function hideAllSlides() {
+            slides.css('display', 'none');
+        }
+
+        progress.css('width', (1000 / slides.length) + 'px');
+
+        function updateProgressBar() {
+            var maxProgressBarPosition = 1000 - (1000 / slides.length);
+            var slideWidth = 100 / (slides.length - 1); 
+            var progressWidth = slideWidth * currentSlideIndex;
+
+            
+            progress.css('left', `${maxProgressBarPosition * (progressWidth/100)}px`);
+        } 
+
+        hideAllSlides();
+        slides.eq(currentSlideIndex).css('display', 'block');
+
+        updateProgressBar();
+
+        progressContainer.click(function(event) {
+            var progressX = progress.offset().left;
+            var clickX = event.clientX;
+            
+            if (clickX < progressX) {
+                prevSlide();
+            } else {
+                nextSlide();
+            }
+
+        });
+    });
+});
+  
+  
+$(document).ready(function() {
+    $('.ct_shop_conteiner').hide();
+    $('.ct_shop_conteiner[data-index="1"]').show();
+
+    $('.tab').click(function() {
+        $('.tab').removeClass('active_ct');
+        var dataIndex = $(this).data('index');
+        
+        $('.ct_shop_conteiner').hide();
+        $('.ct_shop_conteiner[data-index="' + dataIndex + '"]').show();
+        
+        $(this).addClass('active_ct');
+    });
+
+    $('.tab[data-index="1"]').click();
+});
+
+
+$(".ct_shop_sub_conteiner > ul").hide();
+
+$(".ct_shop_sub_conteiner").each(function() {
+    var container = $(this);
+    var containerButton = container.find('div:nth-child(1)');
+    var containerContent = container.find('ul');
+    var containerArrow = containerButton.find('.ct_shop_sub_conteiner_arrow');
+
+    var isContentVisible = false;
+
+    $(containerButton).click(function() {
+        isContentVisible = !isContentVisible;
+        $(containerContent).slideToggle();
+        containerArrow.text(isContentVisible ? 'v' : '>');
+    });
+});
+
+
+$('.ct_shop_sub_reset').click(function(){
+    $(".ct_shop_sub_conteiner > ul").hide();  
+    $('.ct_shop_sub_conteiner_arrow').text('>')
+});
---
+++ Scripts/Index.js
@@ -0,0 +1,190 @@
+$(".i_header_1 > span").click(function () {
+    var $languageContent = $(this).find(".i_language_content");
+    $languageContent.toggle(50);
+});
+
+$(".i_header_1 > span").mouseleave(function () {
+    var $languageContent = $(this).find(".i_language_content");
+    $languageContent.hide(50);
+});
+
+$(".i_language_content").mouseenter(function () {
+    $(this).stop(true, true);
+});
+
+$(".i_main_far > ul > li").each(function() {
+    var $mainImage = $(this).find("a > div:first-of-type > img");
+    var initialSrc = $mainImage.attr('src');
+    $mainImage.attr('data-initial-src', initialSrc);
+
+    $(this).find("a > div:last-of-type > div:first-of-type > div > img").mouseenter(function() {
+        var newImageUrl = $(this).attr('src');
+        $mainImage.attr('src', newImageUrl);
+    });
+
+    $(this).find("a > div:last-of-type > div:first-of-type > div > img").mouseleave(function() {
+        $mainImage.attr('src', initialSrc);
+    });
+});
+
+var subContainers = $('.i_main_li_sub');
+
+subContainers.each(function () {
+    var container = $(this);
+    var imageList = container.find('ul.mar_pad li');
+    var currentIndex = 0;
+    
+    function showImage(index) {
+        imageList.css('display', 'none');
+        imageList.eq(index).css('display', 'block');
+    }
+    
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % imageList.length;
+        showImage(currentIndex);
+    }
+    
+    showImage(currentIndex);
+
+    var interval = setInterval(function () {
+        nextImage();
+    }, 2000);
+    
+    container.on('mouseout', function () {
+        interval = setInterval(function () {
+            nextImage();
+        }, 2000);
+    });
+    
+    container.on('mouseover', function () {
+        clearInterval(interval);
+    });
+});
+
+
+
+$(document).ready(function() {
+    var $ImainContainers = $('.i_main_container');
+
+    $ImainContainers.each(function() {
+        var container = $(this);
+        var slider = container.find('.slider');
+        var slides = slider.children('li');
+        var prevButton = container.find('.slider-container > .prev-button');
+        var nextButton = container.find('.slider-container > .next-button');
+        var dotsContainer = container.find('.i_main_con_sub');
+        var dots = [];
+    
+        let currentSlideIndex = 0;
+        let autoAdvanceTimer;
+    
+        function goToSlide(index) {
+            if (index >= 0 && index < slides.length) {
+                slides.css('display', 'none');
+                slides.eq(index).css('display', 'block');
+            }
+        }
+    
+        function nextSlide() {
+            currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+            goToSlide(currentSlideIndex);
+            updateActiveDot();
+        }
+    
+        function prevSlide() {
+            currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+            goToSlide(currentSlideIndex);
+            updateActiveDot();
+        }
+    
+        nextButton.click(nextSlide);
+        prevButton.click(prevSlide);
+    
+        slider.on('mouseover', function() {
+            clearInterval(autoAdvanceTimer);
+        });
+    
+        prevButton.on('mouseover', function() {
+            clearInterval(autoAdvanceTimer);
+        });
+    
+        nextButton.on('mouseover', function() {
+            clearInterval(autoAdvanceTimer);
+        });
+    
+        slider.on('mouseout', function() {
+            autoAdvanceTimer = setInterval(nextSlide, 2000);
+        });
+    
+        autoAdvanceTimer = setInterval(nextSlide, 2000);
+    
+        function hideAllSlides() {
+            slides.css('display', 'none');
+        }
+    
+        function updateActiveDot() {
+            dotsContainer.find('.dot').removeClass('active-dot');
+            dots[currentSlideIndex].addClass('active-dot');
+        }
+    
+        hideAllSlides();
+        slides.eq(currentSlideIndex).css('display', 'block');
+    
+        slides.each(function(index) {
+            var dot = $('<div class="dot" data-slide-index="' + index + '"></div>');
+    
+            dot.on('mouseover', function() {
+                clearInterval(autoAdvanceTimer);
+            });
+    
+            dot.on('mouseout', function() {
+                autoAdvanceTimer = setInterval(nextSlide, 2000);
+            });
+    
+            dot.on('click', function() {
+                var index = $(this).data('slide-index');
+                currentSlideIndex = index;
+                goToSlide(index);
+                updateActiveDot();
+            });
+    
+            dots.push(dot);
+            dotsContainer.append(dot);
+        });
+    
+        dots[currentSlideIndex].addClass('active-dot');
+    });        
+});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+var browseBycategory = $('.i_main_bbc > ul > li > div > div > a > div');
+browseBycategory.each(function(){
+    var randomColor = getRandomColor();
+    var gradient = `linear-gradient(to top, ${randomColor}, rgba(255, 0, 0, 0))`;
+    
+    $(this).css('background-image', gradient);
+});
+
+$(document).ready(function() {
+    $('.i_main_sh_conteiner[data-index="1"]').show();
+    
+    $('.tab').click(function() {
+        $('.tab').removeClass('active');
+        var dataIndex = $(this).data('index');
+        
+        $('.i_main_sh_conteiner').hide();
+        $('.i_main_sh_conteiner[data-index="' + dataIndex + '"]').show();
+        
+        $(this).addClass('active');
+    });
+});
+
+
